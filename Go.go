583b80a90def63ca4ed07c65f72938e744140176
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 15:03:45 +0600

    Create Go.go

---
+++ Go.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+	switch a {
+	case 1:
+		fmt.Printf("January\n")
+
+	case 2:
+		fmt.Printf("February\n")
+
+	case 3:
+		fmt.Printf("March\n")
+
+	case 4:
+		fmt.Printf("April\n")
+
+	case 5:
+		fmt.Printf("May\n")
+
+	case 6:
+		fmt.Printf("June\n")
+
+	case 7:
+		fmt.Printf("July\n")
+
+	case 8:
+		fmt.Printf("August\n")
+
+	case 9:
+		fmt.Printf("September\n")
+
+	case 10:
+		fmt.Printf("October\n")
+
+	case 11:
+		fmt.Printf("November\n")
+
+	case 12:
+		fmt.Printf("December\n")
+
+	default:
+		fmt.Printf("Invalid input\n")
+	}
+}

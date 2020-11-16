ef8ce9d9722edc7555f977a77a4e8f220d7a673f
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 15:04:41 +0600

    Create  Lua.lua

---
+++ Lua.lua
@@ -0,0 +1,28 @@
+a = io.read("*n")
+if a == 1 then
+    print("January")
+elseif a == 2 then
+    print("February")
+elseif a == 3 then
+    print("March")
+elseif a == 4 then
+    print("April")
+elseif a == 5 then
+    print("May")
+elseif a == 6 then
+    print("June")
+elseif a == 7 then
+    print("July")
+elseif a == 8 then
+    print("August")
+elseif a == 9 then
+    print("September")
+elseif a == 10 then
+    print("October")
+elseif a == 11 then
+    print("November")
+elseif a == 12 then
+    print("December")
+else
+    print("Invalid input")
+end

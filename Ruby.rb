09cc89e201f4191896e47dff7e2f0147654bbbec
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 15:05:41 +0600

    Create Ruby.rb

---
+++ Ruby.rb
@@ -0,0 +1,29 @@
+a = gets().chomp().to_f
+case a
+when 1
+    printf("January\n");
+when 2
+    printf("February\n");
+when 3
+    printf("March\n");
+when 4
+    printf("April\n");
+when 5
+    printf("May\n");
+when 6
+    printf("June\n");
+when 7
+    printf("July\n");
+when 8
+    printf("August\n");
+when 9
+    printf("September\n");
+when 10
+    printf("October\n");
+when 11
+    printf("November\n");
+when 12
+    printf("December\n");
+else
+    printf("Invalid input\n");
+end
